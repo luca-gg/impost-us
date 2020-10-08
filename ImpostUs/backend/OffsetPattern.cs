8b78d942abbc9331405551a62098bbc313dae015
Luca <59053718+luca-gg@users.noreply.github.com>
2020-10-08 19:21:06 +0200

    base push

---
+++ ImpostUs/backend/OffsetPattern.cs
@@ -0,0 +1,10 @@
+﻿namespace ImpostUs
+{
+    public sealed class OffsetPattern
+    {
+        public static string PlayerControlPointer = "GameAssembly.dll+1468910";
+        public static string ShipStatusPointer = "GameAssembly.dll+146885C";
+        public static string AmongUsClientPointer = "GameAssembly.dll+1468840";
+        public static string PlayerControlGetData = "55 8B EC 80 3D 2A FC ??";
+    }
+}
